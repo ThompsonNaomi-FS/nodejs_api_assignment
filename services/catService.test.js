20bddc585a30ee62f11fe61b9a2b81c5ab69138f
Naomi Thompson <NAThompson1@student.fullsail.edu>
2022-06-13 18:33:21 -0500

    Edited test amd mock test files

--- services/catService.test.js
+++ services/catService.test.js
@@ -1,13 +1,12 @@
 const{ catService, catServiceByID } = require('./catService');
 
-jest.mock('./catService.js');
+// jest.mock('./catService.js');
 
 describe("Cat Fact Service Test", () => {
     test("Should return a list of cat facts", async () => {
         const result = await catService();
-        expect(result.data).toHaveLength(10);
-        expect(result.data[8].fact).toEqual("Approximately 24 cat skins can make a coat.");
-        expect(result.data[8].length).toEqual(43);
+        expect(result.data.data[8].fact).toEqual("Approximately 24 cat skins can make a coat.");
+        expect(result.data.data[8].length).toEqual(43);
     });
 
     test("Should return a single cat fact by length(id)", async () => {
